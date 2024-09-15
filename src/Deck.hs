582af1b09d94c74764380da521108f27863d6065
BenjaminJorgensen <Benjamin@jorgensens.id.au>
2024-09-16 01:05:46 +1000

    Getting closer with shuffling cards

--- src/Deck.hs
+++ src/Deck.hs
@@ -1,11 +1,30 @@
 module Deck where
 import qualified Data.Enum as DE
+import System.Random
+import Control.Monad.State
+import System.Random.Stateful (StatefulGen)
 
-data Suit = Diamonds | Hearts | Spades | Clubs deriving (Show, Eq, Bounded, Enum, Read)
-data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Show, Eq, Bounded, Ord, Enum, Read)
-data Card = Card {suit :: Suit, value :: Value}
+data Suit = Diamonds | Hearts | Spades | Clubs deriving (Eq, Bounded, Enum, Read)
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Bounded, Ord, Enum, Read)
+data Card = Card {suit :: Suit, value :: Value} deriving (Eq)
 data Colour = Black | Red | Other deriving (Show, Eq)
 type Deck = [Card]
+
+instance Show Card where
+    show card = show (suit card) ++ show (value card) 
+
+instance Show Suit where
+    show Diamonds = "♦"
+    show Hearts =   "♥"
+    show Spades =   "♠"
+    show Clubs =    "♣"
+
+instance Show Value where
+    show Ace =      "A"
+    show King =     "K"
+    show Queen =    "Q"
+    show Jack =     "J"
+    show v = show $ fromEnum v + 2
 
 -- Gets the colour of a card
 getColour :: Card -> Colour
@@ -19,3 +38,23 @@ newDeck :: Deck
 newDeck = [Card {suit=s, value=v} | 
     s <- [DE.minBound .. DE.maxBound] :: [Suit], 
     v <- [DE.minBound .. DE.maxBound] :: [Value]]
+
+-- SHUFFLES THE GIVEN DECK
+shuffle :: Deck -> State StdGen (Deck)
+shuffle = undefined
+
+-- randomSt :: (RandomGen g, Random a) => State g a
+-- randomSt = state random
+
+-- threeCoins :: (Int,Int,Int) -> State StdGen (Int,Int,Int)
+threeCoins :: StatefulGen g m => (Int,Int,Int) -> g -> m (Int,Int,Int)
+threeCoins (x,y,z) = do
+    a <- random
+    b <- random
+    c <- random
+    return (a+x,b+y,c+z)
+
+-- DRAWS A CARD FROM THE DECK, REMOVING IT,
+-- FAILS IF DECK IS EMPTY
+draw :: Deck -> Maybe Deck
+draw = undefined
--- test/DeckSpec.hs
+++ test/DeckSpec.hs
@@ -2,6 +2,15 @@ module DeckSpec (spec) where
 import Test.Hspec
 import Deck
 
+sumSuit :: Suit -> Deck -> Int
+sumSuit suitType = length . filter ((== suitType) . suit)
+
+-- True if list contains duplicate entries
+isDup :: (Eq a) => [a] -> Bool
+isDup [] = False
+isDup (_:[]) = False
+isDup (x:xs) = if elem x xs then True else isDup xs
+
 suits :: [Suit]
 suits = [Diamonds, Hearts, Clubs, Spades]
 
@@ -12,7 +21,21 @@ spec = do
                (length newDeck) `shouldBe` (length suits)*13
             it "New deck has Correct number of suits" $ do
                 map (`sumSuit` newDeck) suits `shouldBe` replicate (length suits) 13
+            it "Has no duplicates" $ do
+                isDup newDeck `shouldBe` False
 
-sumSuit :: Suit -> Deck -> Int
-sumSuit suitType = length . filter ((== suitType) . suit)
+        describe "Deck Shuffling" $ do
+            it "Deck shuffle changes card order" $ do
+                ((shuffle newDeck) == newDeck) `shouldBe` False
+            it "Deck shuffle does not remove cards" $ do
+               (length (shuffle newDeck)) `shouldBe` (length suits)*13
+            it "Deck shuffle doesn't change suits" $ do
+                map (`sumSuit` (shuffle newDeck)) suits `shouldBe` replicate (length suits) 13
+            it "Deck shuffle doesn't duplicate cards" $ do
+                isDup (shuffle newDeck) `shouldBe` False
+            it "Shuffled deck still has all original cards" $ do
+                any (`elem` newDeck) (shuffle newDeck) `shouldBe` True
 
+        describe "Drawing Cards" $ do
+            it "Deck shuffle changes card order" $ do
+                ((shuffle newDeck) == newDeck) `shouldBe` False
